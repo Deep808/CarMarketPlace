2bf316a52b416e78b71e10b7f3ce49989007005c
Deep <deepprajapati1010@gmail.com>
2024-12-07 18:33:38 -0500

    Added Vehicles Data JSON file

---
+++ frontend/src/utils/vehiclesData.js
@@ -0,0 +1,34 @@
+export const vehiclesData = [
+  {
+    id: 1,
+    name: "Car A",
+    price: 20000,
+    kms: 15000,
+    image:
+      "https://images.unsplash.com/photo-1671418203426-fb8a7a2edca8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    name: "Car B",
+    price: 30000,
+    kms: 20000,
+    image:
+      "https://images.unsplash.com/photo-1671418203426-fb8a7a2edca8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    name: "Car C",
+    price: 15000,
+    kms: 10000,
+    image:
+      "https://images.unsplash.com/photo-1671418203426-fb8a7a2edca8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    name: "Car D",
+    price: 25000,
+    kms: 18000,
+    image:
+      "https://images.unsplash.com/photo-1671418203426-fb8a7a2edca8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
