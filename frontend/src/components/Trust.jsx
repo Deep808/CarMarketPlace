296dc3a5afa8ec2ffae3fc48a20229d5e5d52128
vishalpatel18 <vp186vishalpatel@gmail.com>
2024-12-07 22:17:35 -0500

    updated  trust.jsx component

--- frontend/src/components/Trust.jsx
+++ frontend/src/components/Trust.jsx
@@ -3,16 +3,19 @@ import { brandsData } from "../utils/brandsData";
 
 const Trust = () => {
   return (
-    <div className="max-w-[80%] mx-auto">
-      <h1 className="text-center text-[2em] font-bold">
+    <div className="relative max-w-[80%] md:max-w-[70%] lg:max-w-[60%] mx-auto z-10">
+      {/* Section title */}
+      <h1 className="text-center text-[2em] font-bold mt-12 lg:mt-12 lg:text-[3em]">
         Trusted by Top Companies
       </h1>
 
-      <div className="flex justify-between my-8">
+      {/* Logos of trusted brands */}
+      <div className="max-w-[90%] md:max-w-full mx-auto flex justify-between my-8 md:mt-16">
         {brandsData.map((data) => (
           <img
-            className="w-16 h-16 object-contain"
-            key={data.id}
+            // Each brand logo with hover effect
+            className="w-14 h-14 md:w-24 md:h-24 object-contain hover:scale-105 cursor-pointer transition-all duration-300 ease-in-out"
+            key={data.id} // Unique key for each brand logo
             src={data.image}
             alt="cars-logos"
           />
