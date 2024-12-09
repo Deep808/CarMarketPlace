60d1b16e84bff1651483c96e838fe55cdf5e3c08
Deep <deepprajapati1010@gmail.com>
2024-12-08 19:33:23 -0500

    Updated new mongodb connection cluster

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,29 @@
 // include packages
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const carRoutes = require('./routes/carRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const carRoutes = require("./routes/carRoutes");
 
 const app = express();
 const PORT = 5000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://dmprajapati8585:Deep@cluster0.osjzk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect("mongodb+srv://deep:deep123@cluster0.dyav1.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.use('/api/cars', carRoutes);
+app.use("/api/cars", carRoutes);
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
