f7e54b9ae8669ec58977cea9ede9fe41ec78ab71
Deep2410 <deep24102410@gmail.com>
2024-12-10 01:58:30 -0500

    integrated send email functionality with external api which can send one email per minute

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
 
 const ContactForm = () => {
+
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [username, setName] = useState('');
+  const [showGreetings, setShowGreetings] = useState(false);
+  const [emailInProgress, setEmailInProgress] = useState(false);
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  }
+
+  const sendEmail = async () => {
+    if (email === '' || message === '' || username === '') {
+        alert('Please fill all the details of the Form');
+    } else {
+        try {
+            setEmailInProgress(true);
+            const response = await axios.post('https://resume-backend-9i7r.onrender.com/send-email', { name:username, email, message });
+            console.log(response);
+  
+            if (response.status === 200) {
+                setEmail('');
+                setMessage('');
+                setName('');
+                setShowGreetings(true);
+                setTimeout(() => {
+                    setShowGreetings(false);
+                }, 5000);
+            }
+        } catch (error) {
+            console.error('Error in sending email' + error);
+            alert('There is a problem in sending email');
+        }
+  
+        setEmailInProgress(false);
+    }
+  }
+
   return (
-    <form className="mx-auto bg-white rounded my-6">
+    <form className="mx-auto bg-white rounded my-6" onSubmit={sendEmail}>
       {/* Form Grid Layout */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {/* First Name Input */}
@@ -17,6 +64,8 @@ const ContactForm = () => {
             type="text"
             id="firstName"
             name="firstName"
+            onChange={handleName}
+            value={username}
             placeholder="Your First Name"
             className="border border-[#6A1E55]/20 outline-none p-4 mt-1 block w-full rounded shadow-sm focus:border-[#6A1E55] focus:ring-[#6A1E55]"
           />
@@ -50,6 +99,8 @@ const ContactForm = () => {
           <input
             type="email"
             id="email"
+            onChange={handleEmail}
+            value={email}
             name="email"
             placeholder="Your Email"
             className="border border-[#6A1E55]/20 outline-none p-4 mt-1 block w-full rounded shadow-sm focus:border-[#6A1E55] focus:ring-[#6A1E55]"
@@ -85,6 +136,8 @@ const ContactForm = () => {
         <textarea
           id="message"
           name="message"
+          onChange={handleMessage}
+          value={message}
           placeholder="Write Your Message Here..."
           className="border border-[#6A1E55]/20 outline-none p-4 mt-1 block w-full rounded shadow-sm focus:border-[#6A1E55] focus:ring-[#6A1E55]"
           rows="5"
